48a05e905f6746a36d98830c79b1354f1f60bc2b
Ajay Khandekar <ajaykhandekarklingelnberg@gmail.com>
2024-12-30 10:50:02 +0530

    Add Exception Handling for invalid operation.

--- CalculatorForExceptionHandling/Calculator.cs
+++ CalculatorForExceptionHandling/Calculator.cs
@@ -33,52 +33,59 @@ namespace CalculatorForExceptionHandling
         }
         public void checkOperationType()
         {
-            switch (operation)
+            try
             {
-                case "+":
-                    TakeTwoNumbersInput();
-                    ArithmaticOperations additionOperation = new ArithmaticOperations();
-                    additionOperation.Addition(firstNumber, secondNumber);
-                    break;
-                case "-":
-                    TakeTwoNumbersInput();
-                    ArithmaticOperations subtractionOperation = new ArithmaticOperations();
-                    subtractionOperation.Subtraction(firstNumber, secondNumber);
-                    break;
-                case "*":
-                    TakeTwoNumbersInput();
-                    ArithmaticOperations multiplicationOperation = new ArithmaticOperations();
-                    multiplicationOperation.Multiplication(firstNumber, secondNumber);
-                    break;
-                case "/":
-                    TakeTwoNumbersInput();
-                    ArithmaticOperations divisionOperation = new ArithmaticOperations();
-                    divisionOperation.Division(firstNumber, secondNumber);
-                    break;
-                case "sin":
-                    TakeSingleNumberInput();
-                    TrigonometricOperations sinOperation = new TrigonometricOperations();
-                    sinOperation.Sin(firstNumber);
-                    break;
-                case "cos":
-                    TakeSingleNumberInput();
-                    TrigonometricOperations cosOperation = new TrigonometricOperations();
-                    cosOperation.Cos(firstNumber);
-                    break;
-                case "tan":
-                    TakeSingleNumberInput();
-                    TrigonometricOperations tanOperation = new TrigonometricOperations();
-                    tanOperation.Tan(firstNumber);
-                    break;
-                case "factorial":
-                    TakeSingleNumberInput();
-                    MathOperations factorialoperation = new MathOperations();
-                    Console.WriteLine((int) firstNumber);
-                    factorialoperation.Factorial((int )firstNumber);
-                    break;
-                default:
-                    Console.WriteLine("Invalid operation , Please enter a valid operation.");
-                    break;
+                switch (operation)
+                {
+                    case "+":
+                        TakeTwoNumbersInput();
+                        ArithmaticOperations additionOperation = new ArithmaticOperations();
+                        additionOperation.Addition(firstNumber, secondNumber);
+                        break;
+                    case "-":
+                        TakeTwoNumbersInput();
+                        ArithmaticOperations subtractionOperation = new ArithmaticOperations();
+                        subtractionOperation.Subtraction(firstNumber, secondNumber);
+                        break;
+                    case "*":
+                        TakeTwoNumbersInput();
+                        ArithmaticOperations multiplicationOperation = new ArithmaticOperations();
+                        multiplicationOperation.Multiplication(firstNumber, secondNumber);
+                        break;
+                    case "/":
+                        TakeTwoNumbersInput();
+                        ArithmaticOperations divisionOperation = new ArithmaticOperations();
+                        divisionOperation.Division(firstNumber, secondNumber);
+                        break;
+                    case "sin":
+                        TakeSingleNumberInput();
+                        TrigonometricOperations sinOperation = new TrigonometricOperations();
+                        sinOperation.Sin(firstNumber);
+                        break;
+                    case "cos":
+                        TakeSingleNumberInput();
+                        TrigonometricOperations cosOperation = new TrigonometricOperations();
+                        cosOperation.Cos(firstNumber);
+                        break;
+                    case "tan":
+                        TakeSingleNumberInput();
+                        TrigonometricOperations tanOperation = new TrigonometricOperations();
+                        tanOperation.Tan(firstNumber);
+                        break;
+                    case "factorial":
+                        TakeSingleNumberInput();
+                        MathOperations factorialoperation = new MathOperations();
+                        Console.WriteLine((int)firstNumber);
+                        factorialoperation.Factorial((int)firstNumber);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(operation);
+                        
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Incorrect operation enterd: " + ex.ParamName);
             }
         }
     }
